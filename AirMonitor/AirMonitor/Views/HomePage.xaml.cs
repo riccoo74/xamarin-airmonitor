f95eb7f90fe8164c64fadb17410674d1b53e8473
Krzysztof Ryś <48654364+riccoo74@users.noreply.github.com>
2020-07-18 14:46:13 +0200

    Lab 2 - Wydzielenie styli i stworzenie nawigacji

---
+++ AirMonitor/AirMonitor/Views/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using AirMonitor.ViewModels;
+using Xamarin.Forms;
+
+namespace AirMonitor.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
